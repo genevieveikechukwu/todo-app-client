ba949566b9679bdb80f29dd008c60f0a99ce075b
Genevieve Ikechukwu <ikechukwummachukwu2020@gmail.com>
2023-04-07 13:49:12 +0100

    Ready to deploy frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios'
+import swal from 'sweetalert';
 import './App.css';
 
+
 function App() {
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+  const [category, setCategory] = useState('')
+  const [status, setStatus] = useState('')
+  const addTodo = () => {
+    Axios.post('http://localhost:3001/api/todo/add', {
+      title: title,
+      description: description,
+      category: category,
+      status: status
+    }).then(() => {
+      swal("message", "Todo has been added", "success")
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TODO APP</h1>
+      <div className='form'>
+        <label htmlFor="title">Title</label>
+        <input type="text" name="todo" id="title" onChange={(e) => {
+          setTitle(e.target.value)
+        }} />
+        <label htmlFor="description">Description</label>
+        <input type="text" name="todo" id="description" onChange={(e) => {
+          setDescription(e.target.value)
+        }} />
+        <label htmlFor="category">Category</label>
+        <input type="text" name="todo" id="category" onChange={(e) => {
+          setCategory(e.target.value)
+        }} />
+        <label>
+          <input type="checkbox" name="subscribe" value="1"
+            onChange={(e) => {
+              setStatus(e.target.value)
+            }} />
+        </label>
+
+
+        <button type='submit' onClick={addTodo}>Add</button>
+      </div>
     </div>
   );
 }
